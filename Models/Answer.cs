65583d4ee464f6dfc7a52ab6b870356dc0686662
ArtemNoreyko <noreykoartem@gmail.com>
2022-03-04 16:10:55 +0300

    Add EF and DB

---
+++ Models/Answer.cs
@@ -0,0 +1,18 @@
+﻿namespace Questions_and_Answers_API.Models
+{
+    public class Answer
+    {
+        public Guid Id { get; set; }
+        public string? Text { get; set; }
+        public DateTime TimeOfCreation { get; set; }
+        public DateTime TimeOfUpdate { get; set; }
+        public string? UserId { get; set; }
+        public User? User { get; set; }
+
+        public Guid TagId { get; set; }
+        public Tag? Tag { get; set; }
+
+        public Guid QuestionId { get; set; }
+        public Question? Question { get; set; }
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,18 @@
+﻿namespace Questions_and_Answers_API.Models
+{
+    public class Question
+    {
+        public Guid Id { get; set;}
+        public string? Title { get; set;}
+        public string? Description { get; set;}
+        public DateTime DateOfCreation { get; set; }
+        public DateTime DateOfUpdate { get; set; }
+
+        public string? UserId { get; set; }
+        public User? User { get; set; }
+
+        public Guid TagId { get; set; }
+        public Tag? Tag { get; set; }
+
+    }
+}
